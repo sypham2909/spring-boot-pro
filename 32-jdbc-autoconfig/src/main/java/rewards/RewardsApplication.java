9975d72524e8b902776fc263a9d54cb92d28c586
sypham2909 <sy.buster@gmail.com>
2023-03-14 01:21:39 +0700

    Complete 32-jdbc-autoconfig

--- 32-jdbc-autoconfig/src/main/java/rewards/RewardsApplication.java
+++ 32-jdbc-autoconfig/src/main/java/rewards/RewardsApplication.java
@@ -1,12 +1,15 @@
 package rewards;
 
+import config.RewardsConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 // TODO-00 : In this lab, you are going to exercise the following:
@@ -40,8 +43,9 @@ import org.springframework.jdbc.core.JdbcTemplate;
 // TODO-13 (Optional) : Follow the instruction in the lab document.
 //           The section titled "Build and Run using Command Line tools".
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableConfigurationProperties(RewardsRecipientProperties.class)
+@Import(RewardsConfig.class)
 public class RewardsApplication {
     static final String SQL = "SELECT count(*) FROM T_ACCOUNT";
 
